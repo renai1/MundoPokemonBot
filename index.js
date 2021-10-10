394fec2bf831059ea9cfee4743abb3492b74bf65
Renai <80545089+renai1@users.noreply.github.com>
2021-10-10 06:07:59 +0000

    stuff

--- index.js
+++ index.js
@@ -17,25 +17,25 @@ const fs = require("fs"); //Make sure you add the package.
 client.on("ready", () => {
   client.setMaxListeners(0)
   client.user.setUsername("MundoPokemon")
-  client.user.setPresence({ activity: { name: ",help"}, status: "online" })
+  client.user.setPresence({ activity: { name: ",help" }, status: "online" })
 });
 
 client.commands = new Discord.Collection(); //This is very important so do not forget it!
 const files = fs.readdirSync("./commands/").filter(file => file.endsWith(".js")); //Make sure you add the folder called "commands"
-for(const file of files){
- const command = require(`./commands/${file}`);
- client.commands.set(command.name, command);
+for (const file of files) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
 }
 
 client.on("message", message => {
- if(message.channel.type === "dm") return;
- if(message.author.bot) return;
- if(!message.content.startsWith(prefix)) return;
+  if (message.channel.type === "dm") return;
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
 
- const args = message.content.slice(prefix.length).split(/ +/);
- const cmd = args.shift().toLowerCase();
- const command = client.commands.get(cmd);
- command.callback(client,message,args,Discord);
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const cmd = args.shift().toLowerCase();
+  const command = client.commands.get(cmd);
+  command.callback(client, message, args, Discord);
 });
 
 client.login(process.env.token)
